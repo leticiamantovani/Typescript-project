4e3a219992322db109a41f8b262c5dcb3edd468b
Leticia Mantovani <leticiamantovani159@gmail.com>
2022-04-13 12:40:30 -0300

    adding upload file function

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { request, response, Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/UseCases/CreateCategory';
+import { importCategoryController } from '../modules/cars/UseCases/importCategory';
 import { listCategoriesController } from '../modules/cars/UseCases/ListCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 // eslint-disable-next-line arrow-body-style
 categoriesRoutes.post('/', (request, response) => {
@@ -13,6 +19,11 @@ categoriesRoutes.post('/', (request, response) => {
 // eslint-disable-next-line arrow-body-style
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+// eslint-disable-next-line arrow-body-style
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
